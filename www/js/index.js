b2a6a7a92509f0eb337fb782ed3d0e19230c6796
“jeenitamehta” <jeenita@clevertap.com>
2021-04-30 19:15:40 +0530

    first commit

---
+++ www/js/index.js
@@ -0,0 +1,37 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+// Wait for the deviceready event before using any of Cordova's device APIs.
+// See https://cordova.apache.org/docs/en/latest/cordova/events/events.html#deviceready
+document.addEventListener('deviceready', onDeviceReady, false);
+
+function onDeviceReady() {
+    // Cordova is now initialized. Have fun!
+
+    console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
+    document.getElementById('deviceready').classList.add('ready');
+    CleverTap.onUserLogin({"Name": "jm","Identity": 19962516, "custom":1.3});
+    //CleverTap.setPushToken("752689082317".trim())
+    document.addEventListener('onPushNotification', this.onPushNotification, true);
+     // optional, register to receive push notification payloads
+     CleverTap.createNotificationChannel("hi","hi","This is a test channel",5,true);
+}
+function onPushNotification(){
+    console.log(JSON.stringify(e.notification));
+}
